534849eb659c4f5c121ee44453d906b05fae6c2b
Taci <taciyalcin@gmail.com>
2025-06-09 09:01:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     // Harita Başlangıç Ayarları
     const map = L.map('map').setView([41.0082, 28.9784], 13); // İstanbul merkez koordinatları ve zoom seviyesi
 
-    // Stadia Maps Stamen Toner Lite katmanını ekleme (Siyah beyaz ve çok minimalist - YÜKSEK GÜVENİLİRLİK)
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.png', {
-        minZoom: 0,
-        maxZoom: 20,
-        attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // CartoDB Positron katmanını ekleme (Daha sade ve minimalist görünüm)
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 20
     }).addTo(map);
 
     // Konum butonunu ekleme
