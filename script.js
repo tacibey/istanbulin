e58fe6c096574bf1739e010d8158339834c3965b
Taci <taciyalcin@gmail.com>
2025-06-09 08:02:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const map = L.map('map').setView([41.0082, 28.9784], 13); // İstanbul merkez koordinatları ve zoom seviyesi
 
     // OpenStreetMap katmanını ekleme
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 20
+}).addTo(map);
 
     // Özel "i" ikonunu tanımlama
     const customMarkerIcon = L.divIcon({
