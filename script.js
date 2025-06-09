4ad083d05b777b9bade5ca09ebb891fd69d31bec
Taci <taciyalcin@gmail.com>
2025-06-09 08:49:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     // Harita Başlangıç Ayarları
     const map = L.map('map').setView([41.0082, 28.9784], 13); // İstanbul merkez koordinatları ve zoom seviyesi
 
-    // OpenStreetMap katmanını ekleme (Tekrar Güvenilir Seçenek)
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // CartoDB Positron katmanını ekleme (Daha sade ve minimalist görünüm)
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 20
     }).addTo(map);
 
     // Konum butonunu ekleme
