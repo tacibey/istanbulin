8adaea5ff25f3867d148baff1a3cbcf532fda072
Taci <taciyalcin@gmail.com>
2025-06-10 19:12:42 +0300

    Create main-script.js

---
+++ main-script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger-menu');
+    const navLinks = document.getElementById('nav-links');
+
+    if (hamburger && navLinks) {
+        hamburger.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+            hamburger.classList.toggle('active');
+        });
+    }
+});
