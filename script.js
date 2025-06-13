95e7f958a985d840e9840ce60870a31ad57387c5
Taci <taciyalcin@gmail.com>
2025-06-13 15:15:11 +0300

    Update script.js [skip ci]

--- script.js
+++ script.js
@@ -77,8 +77,8 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     
     const darkTheme = L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-        maxZoom: 19, // Yine de 19'a kadar gitmesine izin ver (dijital zoom)
-        maxNativeZoom: 19, // Ama gerçek resimlerin 16'da bittiğini söyle
+        maxZoom: 16, // 
+        maxNativeZoom: 16, // 
         noWrap: true
     });
 
